1e45cba453ea54874cb5c199947765fbf0b7bdd7
Chaudhary97 <89289599+Chaudhary97@users.noreply.github.com>
2023-04-24 08:40:56 +0500

    Update coordinateGeometry.py

--- coordinateGeometry.py
+++ coordinateGeometry.py
@@ -1,3 +1,4 @@
+from math import sqrt
 cord1 = input("Enter first coordinate seperated by ',' (syntax:(x, y): ")
 cord1 = cord1.split(",")
 cord1_x = int(cord1[0])
@@ -10,7 +11,12 @@ cord2 = cord2.split(",")
 cord2_x = int(cord2[0])
 cord2_y = int(cord2[1])
 
-res_x = (cord1_x + cord2_x) / 2
-res_y = (cord1_y + cord2_y) / 2
+x = int(input("1(Distance Formula) / 2(Mjdpoint Formula) : "))
 
-print(f"{res_x},{res_y}")
+if x == 1:
+	y = sqrt(((cord2_x-cord1_x)**2)+((cord2_y-cord1_y)**2))
+	print(f"The distance b/w ({cord1_x},{cord1_y}) & ({cord2_x},{cord2_y}) is {y}")
+elif x == 2:
+	res_x = (cord1_x + cord2_x) / 2
+	res_y = (cord1_y + cord2_y) / 2
+	print(f"The midpoint of ({cord1_x},{cord1_y}) & ({cord2_x},{cord2_y}) is ({res_x},{res_y})")
